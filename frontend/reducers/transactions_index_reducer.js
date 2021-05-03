998d1736441169d39e8bfc6e1c26bc8d096d8f85
Julian <tipler.julian@gmail.com>
2021-05-03 14:46:05 -0700

    set up grid

--- frontend/reducers/transactions_index_reducer.js
+++ frontend/reducers/transactions_index_reducer.js
@@ -4,6 +4,7 @@ const transactionsIndexReducer = (oldState = [], action) => {
     Object.freeze(oldState);
     switch (action.type) {
         case RECEIVE_TRANSACTIONS:
+            console.log(action.transactions)
             return action.transactions
         default:
             return oldState;
