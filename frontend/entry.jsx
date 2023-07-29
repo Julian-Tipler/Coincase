568d1a93adab8a40b77a27c00cc996c6fb515492
Julian <tipler.julian@gmail.com>
2023-07-29 13:22:33 -0500

    logging entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -7,7 +7,9 @@ import * as userApiActions from './actions/user_actions'
 import Root from './components/root'
 
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("here")
     const root = document.getElementById("root");
+    console.log("root", root)
     var store = null
     if (window.currentUser){
         const preloadedState = {
@@ -23,6 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     window.geckoApiActions = geckoApiActions
     window.userActions = userApiActions
     window.store = store
+    console.log(window.getState, window.geckoApiActions, window.userActions, window.store)
     
     ReactDOM.render(<Root store={store}/>, root);
 });
